68f5a952146344cdd4bbee7ad4124ac63f2ab722
Your Name <you@example.com>
2021-12-09 23:05:49 -0800

    initial commit

---
+++ src/test/java/devopsLearning/Test1.java
@@ -0,0 +1,28 @@
+package devopsLearning;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void testOne() {
+		Assert.assertEquals(false, false);
+	}
+
+	@Test
+	public void testO1ne() {
+		Assert.assertEquals(false, false);
+	}
+
+	@Test
+	public void test2One() {
+		Assert.assertEquals(false, false);
+	}
+
+	@Test
+	public void test3One() {
+		Assert.assertEquals(false, false);
+	}
+
+}
